c4dbe2b9a8db4a93e08fbf35636783f5482479ff
OluwadamilolaOlaniyan <oluwadamilola.olaniyan@gmail.com>
2026-02-25 00:46:44 +0100

    updated API key

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBzf8ma9Bs0qcK2pZkl_hY5BSjweFjNgSM",
+  apiKey: "AIzaSyA4i-nWmuqixUZKD1cuu1FSMiFotLMExk4",
   authDomain: "fitness-tracker-applicat-1a8a4.firebaseapp.com",
   projectId: "fitness-tracker-applicat-1a8a4",
   storageBucket: "fitness-tracker-applicat-1a8a4.firebasestorage.app",
