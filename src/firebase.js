265b9305ded817afba8d41a7d1464ec320c818fb
OluwadamilolaOlaniyan <oluwadamilola.olaniyan@gmail.com>
2026-02-25 21:19:49 +0100

    updated firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA4i-nWmuqixUZKD1cuu1FSMiFotLMExk4",
+  apiKey: "AIzaSyAR7PYCwE16zMhNa4P-KWRwbg73bGtjeL0",
   authDomain: "fitness-tracker-applicat-1a8a4.firebaseapp.com",
   projectId: "fitness-tracker-applicat-1a8a4",
   storageBucket: "fitness-tracker-applicat-1a8a4.firebasestorage.app",
   messagingSenderId: "621967991528",
   appId: "1:621967991528:web:ee4e0e843606cde56d1f5d",
+  measurementId: "G-ZRL12K61CE"
 };
 
 const app = initializeApp(firebaseConfig);
